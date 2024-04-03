e8fa9ccabc2ef6391071e7999d53048d8debe080
aminhavo <117644068+aminhavo@users.noreply.github.com>
2024-04-03 09:36:51 -0300

    Update Mais.js

--- Mais.js
+++ Mais.js
@@ -19,10 +19,10 @@ for (let i = 0; i < botoes.length; i++) {
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-10T10:30:00");
+const tempoObjetivo2 = new Date("2024-09-10T12:30:00");
+const tempoObjetivo3 = new Date("2024-10-30T02:40:00");
+const tempoObjetivo4 = new Date("2024-10-01T02:00:00");
 
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
